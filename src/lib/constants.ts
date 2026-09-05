48c5468202c3b6e3f76e0b18668f126d434a69d6
intelli-ia <intelliai.projetos@gmail.com>
2026-09-05 11:42:31 -0300

    chore: update CTA_LINK product offer ID

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const CTA_LINK =
-  "https://pay.zouti.com.br/checkout?product_offer_id=prod_offer_hlepg99b4ojgxcvt6y0wr0";
+  "https://pay.zouti.com.br/checkout?product_offer_id=prod_offer_04uqeryv4ccsbtv8mvin1i";
 
 export const SITE_NAME = "MAPA";
 export const PRODUCT_NAME = "MAPA";
